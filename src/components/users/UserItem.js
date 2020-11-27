67820e21ecaab173b4564e6330b6106a64f71606
OssaiJE <sirjuliusdev@gmail.com>
2020-11-27 23:56:54 +0100

    started working on a github developers finder using the github api with react

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export class UserItem extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from UserItem.js</h1>
+            </div>
+        )
+    }
+}
+
+export default UserItem;
