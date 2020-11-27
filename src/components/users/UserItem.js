b8c3fc012ae005a1d1631c46f8190e49d699db87
OssaiJE <sirjuliusdev@gmail.com>
2020-11-28 00:22:34 +0100

    updated the readme and the UserItem.js file

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 
 
 export class UserItem extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            id: "id",
+            login: "mojombo",
+            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+            html_url: "https://api.github.com/users/mojombo"
+        }
+    }
     render() {
         return (
             <div>
