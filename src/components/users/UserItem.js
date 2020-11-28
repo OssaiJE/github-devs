ff1b5fed9df33c0a1caf882ab5decbc686388bfd
OssaiJE <sirjuliusdev@gmail.com>
2020-11-28 02:23:16 +0100

    was able to get the UserItem.js and Users.js to work

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,20 +2,15 @@ import React, { Component } from 'react';
 
 
 export class UserItem extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            id: "id",
-            login: "mojombo",
-            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url: "https://api.github.com/users/mojombo"
-        }
-    }
     render() {
+        const { login, avatar_url, html_url} = this.props.user;
         return (
-            <div>
-                <h1>Hello from UserItem.js</h1>
+            <div className="card text-center">
+                <img src={avatar_url} alt="Avatar" className="round-img" style={{ width: "60px"}}  />
+                <h3>{login}</h3>
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+                </div>
             </div>
         )
     }
