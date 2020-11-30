c069a427dfb2a12d321418bb00c9b1df10dd369d
OssaiJE <sirjuliusdev@gmail.com>
2020-11-30 03:39:45 +0100

    was able to add the react router and routed to the about page and it works perfectlty

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About this web app</h1>
+            <p>This is an app that is used primarily to find gihub developers</p>
+            <p><b>Version: 0.1.0</b></p>
+        </Fragment>
+    )
+}
+
+export default About;
+
