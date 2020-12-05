66c35aecfbcc7d6ee699afbdc9b39868ec44aeb8
OssaiJE <sirjuliusdev@gmail.com>
2020-12-05 03:02:58 +0100

    Moved the home route to Home.js and created the 404 not found page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Fragment, /*useEffect*/ } from "react"
+import React, { /*useEffect*/ } from "react"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import About from './components/pages/About';
-
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertSate';
 
@@ -37,16 +36,12 @@ const App = () => {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert />
           <Switch>
-            <Route exact path="/" render={props => (
-              <Fragment>
-                <Alert />
-                <Search />
-                <Users />
-              </Fragment>
-            )} />
+            <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
             <Route exact path="/user/:login" component={User} />
+            <Route component={NotFound} />
           </Switch>
         </div>
       </div>
